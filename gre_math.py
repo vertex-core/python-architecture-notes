0ba0c782afae7501ccbe9717c6ebcc931a5b22fa
Shamil Asadullayev <flareff11@gmail.com>
2026-09-17 12:59:49 +0400

    Delete gre_math.py

--- gre_math.py
+++
@@ -1,7 +0,0 @@
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-trace = 0
-anti_trace = 0
-for i in range(len(matrix)):
-    trace = matrix[i][i]
-    anti_trace = matrix[i][2 - i]
-    print(anti_trace)
